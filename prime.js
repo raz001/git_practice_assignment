695dd164efae2351f41f8418e04afdea16da5369
raz001 <razbhagat1@gmail.com>
2022-09-27 23:42:48 +0530

    prime logic

--- prime.js
+++ prime.js
@@ -1,1 +1,14 @@
-p
+ function primecheck(num){
+   count=0
+   for(i=1;i<=num;i++){
+     if(num%i==0){
+       count++
+     }
+   }
+   if(count==2){
+        console.log('Prime')
+   } else {
+     console.log("Not")
+   }
+ }
+primecheck(1);
